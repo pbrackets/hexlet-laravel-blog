4c9ebeb2aec75ceaa5e27d968a0ff46934b8aa5a
aelita <ai2work@yandex.ru>
2024-06-04 18:23:31 +0500

    макеты(Layout)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
 
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
+<!-- Хранится в resources/views/about.blade.php -->
+
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
