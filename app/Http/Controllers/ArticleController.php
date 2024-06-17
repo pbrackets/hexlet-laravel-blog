65044c4ba629253c78be412c5ce17ee750648d1d
aelita <ai2work@yandex.ru>
2024-06-17 12:49:30 +0500

    создание (CRUD)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -21,5 +21,37 @@ class ArticleController extends Controller
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
+
+
+    // Вывод формы
+    public function create()
+    {
+        // Передаем в шаблон вновь созданный объект. Он нужен для вывода формы
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    // Здесь нам понадобится объект запроса для извлечения данных
+    public function store(Request $request)
+    {
+
+        // Проверка введенных данных
+        // Если будут ошибки, то возникнет исключение
+        // Иначе возвращаются данные формы
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($data);
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут
+        return redirect()
+            ->route('articles.index');
+    }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
 // имя маршрута, нужно для того, чтобы не создавать ссылки руками
 
+Route::get('articles/create', 'ArticleController@create')
+    ->name('articles.create');
+
+// POST-запрос
+Route::post('articles', 'ArticleController@store')
+    ->name('articles.store');
+
+
 Route::get('articles/{id}', [ArticleController::class, 'show'])
     ->name('articles.show');
 
